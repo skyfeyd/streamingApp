716e25534a1d3aa38f97a9baac7eca3dc497b922
Raphael Castelnau <raphcastel14@gmail.com>
2022-07-21 00:38:15 +0200

    :rocket: start leftSideBar Component

--- src/Components/LeftSideBar/Left.tsx
+++ src/Components/LeftSideBar/Left.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react';
+
+import { HomeIcon } from '../../Assets/pictures/index';
+import { AlbumIcon } from '../../Assets/pictures/index';
+import { TracksIcon } from '../../Assets/pictures/index';
+import { GenreIcon } from '../../Assets/pictures/index';
+
 import './Left.less';
 
 function left() {
   return (
     <section className='left'>
-      <h1>Left</h1>
+      <h2>Browse</h2>
+      <ul className='Browse'>
+        <li><a href=""><img src={ HomeIcon } alt=''/>Home</a></li>
+        <li><a href=""><img src={ AlbumIcon } alt=''/>Albums</a></li>
+        <li><a href=""><img src={ TracksIcon } alt=''/>Track</a></li>
+        <li><a href=""><img src={ GenreIcon } alt=''/>Genre</a></li>
+      </ul>
+      <h2>Librairy</h2>
+      <ul className='Librairy'>
+        <li><a href="">Playlists</a></li>
+        <li><a href="">Recently Played</a></li>
+        <li><a href="">Favorite Tracks</a></li>
+        <li><a href="">Charts</a></li>
+        <li><a href="">Radio</a></li>
+      </ul>
     </section>
   );
 }
