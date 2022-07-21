3a242f019149cff90fa5f1fb766f9f8f58c52a4c
raphael castelnau <raphcastel14@gmail.com>
2022-07-21 10:44:55 +0200

    :rocket: add leftsideBar

--- src/Components/LeftSideBar/Left.tsx
+++ src/Components/LeftSideBar/Left.tsx
@@ -15,9 +15,9 @@ function left() {
         <li><a href=""><img src={ HomeIcon } alt=''/>Home</a></li>
         <li><a href=""><img src={ AlbumIcon } alt=''/>Albums</a></li>
         <li><a href=""><img src={ TracksIcon } alt=''/>Track</a></li>
-        <li><a href=""><img src={ GenreIcon } alt=''/>Genre</a></li>
+        <li><a href=""><img className='genreIcon' src={ GenreIcon } alt=''/>Genre</a></li>
       </ul>
-      <h2>Librairy</h2>
+      <h2 className='LibrairyTitle'>Librairy</h2>
       <ul className='Librairy'>
         <li><a href="">Playlists</a></li>
         <li><a href="">Recently Played</a></li>
